9a533545f924705222144376c4daf301d29feafb
Campbell <mastercamnz@gmail.com>
2025-05-15 21:31:57 +1200

    updates

--- Attempt 2/enviro.py
+++ Attempt 2/enviro.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import random
 class enviroment():
     def __init__(self):
         self.enviro = \
@@ -9,8 +9,8 @@ class enviroment():
          [-1, -1, -1, -1, -1, -9, -1],
          [-1, -9, -9, -9, -9, -9, +9]]
         
-def isTerminal(board):
-    board
+def isTerminal(maze):
+    maze
 
 def allLegalMoves(maze, location):
     move = location.split(",")
@@ -39,10 +39,27 @@ def calculateQScore(qtable):
     discount_factor = 1
     learning_rate = 1
 
-def mazeToState(board):
-    board
+def mazeToState(stateArray, location):
+    move = location.split(",")
+    row = int(move[0])
+    column = int(move[1])
+    return stateArray[row][column]
+
+def chooseAction(maze, epsilon):
+    r = random.uniform(0, 1)
+class createStateArray():
     
+    def __init__(self, board):
+        c=0
+        self.stateArray = [[0 for _ in range(len(board[0]))] for _ in range(len(board))]
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                self.stateArray[i][j] = c
+                c+=1
+
+
 class Qtable():
     def __init__(self):
         self.qtable = np.zeros((35,4))
+
 
